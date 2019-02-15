724bd98cb632b0d7fda8254300447c69d55d1f22
Unknown <ivandanilovich99@gmail.com>
2019-02-15 17:30:49 +0700

    task1

---
+++ Flight/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Flight
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.sControl = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.aControl = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.hControl = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.sControl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aControl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hControl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.sControl);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.aControl);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.hControl);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(749, 128);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(213, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 90);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // sControl
+            // 
+            this.sControl.Location = new System.Drawing.Point(69, 79);
+            this.sControl.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.sControl.Name = "sControl";
+            this.sControl.Size = new System.Drawing.Size(120, 20);
+            this.sControl.TabIndex = 6;
+            this.sControl.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "speed";
+            // 
+            // aControl
+            // 
+            this.aControl.Location = new System.Drawing.Point(69, 45);
+            this.aControl.Maximum = new decimal(new int[] {
+            90,
+            0,
+            0,
+            0});
+            this.aControl.Name = "aControl";
+            this.aControl.Size = new System.Drawing.Size(120, 20);
+            this.aControl.TabIndex = 4;
+            this.aControl.Value = new decimal(new int[] {
+            45,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "angle";
+            // 
+            // hControl
+            // 
+            this.hControl.Location = new System.Drawing.Point(69, 12);
+            this.hControl.Name = "hControl";
+            this.hControl.Size = new System.Drawing.Size(120, 20);
+            this.hControl.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "heigth";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.Maximum = 100D;
+            chartArea1.AxisX.MaximumAutoSize = 2F;
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.AxisY.Maximum = 100D;
+            chartArea1.AxisY.Minimum = 0D;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chart1.Location = new System.Drawing.Point(0, 128);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(749, 322);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 450);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.sControl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aControl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hControl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown sControl;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown aControl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown hControl;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ Flight/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flight
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        const double g = 9.81, dt = 0.1;
+        double t = 0, x = 0, y = 0, y0, alpha, v;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            t += dt;
+            x = v * Math.Cos(alpha * Math.PI / 180) * t;
+            y = y0 + v * Math.Sin(alpha * Math.PI / 180) * t - g * t * t / 2;
+            chart1.Series[0].Points.AddXY(x, y);
+            if (y < 0) timer1.Stop();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            t = 0;
+            chart1.Series[0].Points.Clear();
+
+            v = (double)sControl.Value;
+            alpha = (double)aControl.Value;
+            y0 = (double)hControl.Value;
+            chart1.Series[0].Points.AddXY(0, y0);
+
+            if (!timer1.Enabled) timer1.Start();
+
+        }
+    }
+}
